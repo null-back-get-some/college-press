0bc31cdc7dc4743cb2a302c4cbe11c89502e8605
sooob5567 <81897058+sooob5567@users.noreply.github.com>
2022-09-19 00:36:35 +0900

    컨트롤러 만들고 네비에 연결

---
+++ press/src/main/java/kr/inha/technical/college/press/controller/IndexController.java
@@ -0,0 +1,39 @@
+package kr.inha.technical.college.press.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/univ")
+	public String univ() {
+		return "univ";
+	}
+	
+	@GetMapping("/social")
+	public String social() {
+		return "social";
+	}
+	
+	@GetMapping("/culture")
+	public String culture() {
+		return "culture";
+	}
+	
+	@GetMapping("/picture")
+	public String picture() {
+		return "picture";
+	}
+	
+	@GetMapping("/paper")
+	public String paper() {
+		return "paper";
+	}
+
+}
