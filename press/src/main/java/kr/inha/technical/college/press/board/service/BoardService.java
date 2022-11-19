49ceb40415b9c8a1355bc483f4c3c3d521bd22fb
rueeeeeeng <qhaud7023@naver.com>
2022-11-20 01:54:06 +0900

    qboard 정리

--- press/src/main/java/kr/inha/technical/college/press/board/service/BoardService.java
+++ press/src/main/java/kr/inha/technical/college/press/board/service/BoardService.java
@@ -10,11 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import kr.inha.technical.college.press.board.repository.BoardRepository;
 import kr.inha.technical.college.press.manager.entity.Board;
-import kr.inha.technical.college.press.manager.entity.QBoard;
 
 @Service
 public class BoardService {
@@ -37,21 +35,6 @@ public class BoardService {
 		Optional<Board> list = repository.findById(news);
 		return list;
 	}
-	
-	public List<Board> findByCategory(String category, String subcategory, Pageable pageable){
-		QBoard qBoard = QBoard.board;
-		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
-		
-		List<Board> list=queryFactory.selectFrom(qBoard)
-				.where(qBoard.category.like(category))
-				.where(qBoard.subcategory.like(subcategory))
-				.offset(pageable.getOffset()) 
-				.limit(pageable.getPageSize())
-				.orderBy(qBoard.news.desc())
-				.fetch();
-		return list;
-	}
-
 	
 	public Page<Board> findByCategoryAndSubcategory(int category, String subcategory, Pageable pageable) {
 		return repository.findByCategoryAndSubcategory(category,subcategory, pageable);
